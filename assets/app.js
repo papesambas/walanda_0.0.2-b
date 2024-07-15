1c29ce3766ee4be8f11d8cfd6e3568c4c233f07e
Pape Samba SIDIBE <papesambas@yahoo.fr>
2024-07-15 21:16:36 +0200

    installation de jquery

--- assets/app.js
+++ assets/app.js
@@ -5,6 +5,16 @@ import './bootstrap.js';
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
+import $ from 'jquery';
+import '../assets/scripts/test.js';
+// things on "window" become global variables
+//window.$ = $;
 import './styles/app.css';
+
+// require jQuery normally
+//const $ = require('jquery');
+
+// create global $ and jQuery variables
+// global.$ = global.jQuery = $;
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
